a0156b96ef5697f8c5303551b6269e3f91354ea9
mkdh <mkdh321@yahoo.com.tw>
2017-06-11 00:08:44 +0800

    [update] add power function
    [update] add power function

--- ultrasonic_bebebe_servo.ino
+++ ultrasonic_bebebe_servo.ino
@@ -2,10 +2,10 @@
 //1. not attack when the bebebe is been mutex
 //2. up/down is control servo
 //3. BE~~mode is not good. It will bebebe when no thing is in front of my ultrasonic.
+// close anti-virs when can't change some file , ex:  rename core/core.a
 
 #include "IRremote.h"
 #include <Servo.h>
-
 
 int receiver = 11; // Signal Pin of IR receiver to Arduino Digital Pin 11
 IRrecv irrecv(receiver);           // create instance of 'irrecv'
@@ -13,6 +13,7 @@ decode_results results;            // create instance of 'decode_results'
 
 Servo servo_triggor;
 int current_angle = 0;
+
 
 #define TrigPin 2
 #define EchoPin 3
@@ -24,6 +25,20 @@ int MODE_BEBE = 1;
 #define UPPER_BOUND 179
 #define LOWER_BOUND 0
 int count_shoot = 0;
+int shoot_upper_bound = 50;
+
+int filter_long_be_state = 0;
+
+bool b_power_is_on = true;
+
+void test_shoot_time()
+{
+  for(int i = 0; i < shoot_upper_bound / 10; i++)
+  {
+    delay(500);
+    be();
+  }
+}
 
 void shoot()
 {
@@ -60,22 +75,33 @@ void be()
    delay(20);
    digitalWrite(spk, 0);
 }
+void be_long()
+{
+   digitalWrite(spk, 1);
+   delay(100);
+   digitalWrite(spk, 0);
+}
 
 void bebebe(float sound_distance)
 { 
   //Serial.print((int)Value_cm);
   //Serial.println("cm");
   if(sound_distance > DETECT_DISTANCE)
-  {
-    digitalWrite(spk, 0);
+  {    
+    digitalWrite(spk, 0);   
     count_shoot = 0;
+    filter_long_be_state = 0;
     return;
   }
   else
   {
     if(MODE_BEBE % 2 == 1)
     {
-        digitalWrite(spk, 1);
+        if(b_bebebe)
+        {
+          digitalWrite(spk, 1);
+        }
+     
         if(sound_distance > 10)
         {// BE BE BE...
           delay(5 * (int)sound_distance);
@@ -84,16 +110,22 @@ void bebebe(float sound_distance)
     }
     else
     {//BE~~~~~~~~~~
-      digitalWrite(spk, 1);
+      filter_long_be_state++;
+      if( filter_long_be_state > 3 )
+      {
+        if(b_bebebe)
+        {
+          digitalWrite(spk, 1);
+        }
+      }
+
       count_shoot ++;
-      if(count_shoot == 50)
+      if(count_shoot >= shoot_upper_bound)
       {
         shoot();
       }
-
     }
-      Serial.println(count_shoot);
-
+    Serial.println(count_shoot);
   }
 }
 
@@ -113,18 +145,17 @@ void setup()
 
 void loop()
 {
-  digitalWrite(TrigPin, LOW); //低高低电平发一个短时间脉冲去TrigPin
-  delayMicroseconds(2);
-  digitalWrite(TrigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(TrigPin, LOW);
-  Value_cm = float( pulseIn(EchoPin, HIGH) * 17 )/1000; //将回波时间换算成cm
-  //接收到的高电平的时间（us）* 340m/s / 2 = 接收到高电平的时间（us） * 17000 cm / 1000000 us = 接收到高电平的时间 * 17 / 1000  (cm)
-
-  if(b_bebebe)
+  if(b_power_is_on)
   {
+    digitalWrite(TrigPin, LOW); //低高低电平发一个短时间脉冲去TrigPin
+    delayMicroseconds(2);
+    digitalWrite(TrigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(TrigPin, LOW);
+    Value_cm = float( pulseIn(EchoPin, HIGH) * 17 )/1000; //将回波时间换算成cm
+    //接收到的高电平的时间（us）* 340m/s / 2 = 接收到高电平的时间（us） * 17000 cm / 1000000 us = 接收到高电平的时间 * 17 / 1000  (cm)
     bebebe(Value_cm);
-  }    
+  }  
   delay(100);  
   
   if (irrecv.decode(&results)) // have we received an IR signal?
@@ -141,6 +172,20 @@ void translateIR() // takes action based on IR code received
 {
   Serial.println(results.value, HEX);      
   hex_current_cmd = String(results.value);
+
+
+  
+  if(hex_current_cmd == String(0x514AEB) )
+  {
+    b_power_is_on = !b_power_is_on;
+    be();
+  }
+
+
+  if(b_power_is_on == false)
+  {
+    return;
+  }
   
   if(hex_current_cmd == String(0xFFFFFFFF) )
   {
@@ -162,14 +207,14 @@ void translateIR() // takes action based on IR code received
   
   if(hex_current_cmd ==  String(0x511AEE) )
   {
-    b_bebebe=true;    
+    b_bebebe = true;    
     MODE_BEBE++;    
     be();
   }
   
   if(hex_current_cmd ==  String(0x500AFF) )
   {
-    b_bebebe=false;    
+    b_bebebe = false;    
     be();
   }
   
@@ -183,29 +228,42 @@ void translateIR() // takes action based on IR code received
   {
     add_delta_degree_to_servo(-10);   
     be();
-  }
-  
-  hex_previous_cmd = hex_current_cmd;
+  } 
 
-  /*
-
-
- 
-
-  if(results.value == 0x500AFF)
+  if(hex_current_cmd == String(0x518AE7) )
   {
-    add_delta_degree_to_servo(10);   
+    shoot_upper_bound = shoot_upper_bound + 10;   
     be();
   }
 
-  if(results.value == 0x500AFF)
+  if(hex_current_cmd == String(0x538AC7) )
   {
-    add_delta_degree_to_servo(-10);   
-    be();
+    if( shoot_upper_bound - 10 > 0)
+    {
+      shoot_upper_bound = shoot_upper_bound - 10;   
+      be();
+    }
+    else
+    {
+      shoot_upper_bound = 1;
+      be();
+      delay(50);
+      be();
+      delay(50);
+      be();
+    }    
+  }
+  if(hex_current_cmd == String(0x50EAF1) )
+  {
+    be_long();
+    test_shoot_time();
   }
   //Serial.println(servo.read(), DEC);
-
+ /*
   */
+  hex_previous_cmd = hex_current_cmd;
+
+ 
 /*
   switch(results.value)
 
